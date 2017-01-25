c0d00a46cf1462bf19f423c38a50e067141d5b76
Usagi Ishiko <usagisoftware@gmail.com>
2017-01-25 21:27:21 +0900

    1. Game setup, Player and Camera - 05. Moving the player
    -Added 'PlayerController' script to the 'Player' GameObject.
    -Wrote code to allow for player movement
    -Wrote code to allow for limiting player movement within the game play area
    -Moved the boundary variables to limit the player movement into its own class, 'Boundary'
    -Wrote code to tilt the 'Player' GameObject when it is moving left and right (on the Z axis)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]   // Visible in the Inspector
+public class Boundary
+{
+    public float xMin;
+    public float xMax;
+    public float zMin;
+    public float zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;   // Local Member Variable to hold the Rigidbody Reference
+
+    public float speed = 1;     // Control the ship's speed
+    public float tilt = 1;      // Control the amount of tilting the craft does
+    public Boundary boundary;   // Instance of Boundary
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); // Access the local RigidBody component
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // FixedUpdate is called automatically after each fixed physics stepped.
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); // Store Player Horizontal Movement input in variable
+        float moveVertical = Input.GetAxis("Vertical");     // Store Player Verticle Movement input in variable
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);    // Get 
+
+        rb.velocity = movement * speed;
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+}
