d08ed1bdf9c0bde14b5e4a4f4c9f9e437c32affb
Uasgi Ishiko <usagisoftware@gmail.com>
2017-01-26 11:33:02 +0900

    2. Boundaries, Hazards and Enemies - 01. Boundary
    -Added 'Boundary' Cube GameObject
    -Removed 'Boundary' GameObject's Mesh Filter and Mesh Renderer
    -Set 'Boundary' GameObject's Box Collider to IsTrigger.
    -Created on 'Boundary' a 'Destroy by Boundary' Script to destroy all GameObjects that exit the Boundary's dimensions
    
    Bug Fixes
    -Modified 'PlayerController' Script to fix 'Shot' GameObject not being spawned at the 'Shot Spawn' transform and rotational coordinates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -39,7 +39,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
-            Instantiate(shot, shotSpawn, shotSpawn);
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
         }
     }
 
