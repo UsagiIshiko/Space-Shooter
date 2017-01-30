e317e71899be23c1cdc86e0e4add5fae14b4c2bc
Uasgi Ishiko <usagisoftware@gmail.com>
2017-01-30 10:31:13 +0900

    2. Boundaries, Hazards and Enemies - 02. Creating hazards
    -Added 'Asteroid' Empty GameObject
    -Added 'prop_asteriod_01' model as a child of the 'Asteroid' GameObject
    -Added 'Rigidbody' to 'Asteroid' GameObject
    -Set 'Asteroid' GameObject's gravity to off
    -Set 'Asteroid' Angular Velocity to 0 so that it doesn't stop spinning
    -Added 'Capsule Collider' to 'Asteroid' GameObject and modified to to fit within the object's area.
    -Added 'RandomRotator' Script to 'Asteroid' GameObject to allow it to spin randomly
    -Added 'DestroyByContact' Script to 'Asteroid' GameObject to let laser bolts destroy the 'Asteroid' GameObject on contact
    
    Bug Fixes
    -Changed 'Boundary' GameObject tag to 'Boundary' to allow it to be ignored by the 'DestroyByContact' script.
    
    Updates
    -Unity Editor was upgraded to 5.5.1f1. Project was updated accordingly.

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.name);        // Detects what GameObject is hitting the collider
+        if (other.tag == "Boundary")    // If other GameObject has the 'Boundary' tag
+        {
+            return;                     // End function here.
+        }
+        Destroy(other.gameObject);      // Destroys the GameObject that hits the collider (the Asteroid)
+        Destroy(gameObject);            // Destroys the GameObject itself (the Asteroid itself)
+    }
+}
