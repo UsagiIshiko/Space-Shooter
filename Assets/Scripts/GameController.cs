8c730b0c0df789253f9a81a56dc10a1bf39f3d26
Uasgi Ishiko <usagisoftware@gmail.com>
2017-01-31 10:52:42 +0900

    2. Boundaries, Hazards and Enemies - 04. Game Controller
    -Created 'Game Controller' Empty GameObject
    -Created 'GameController' Script on 'Game Controller' GameObject
    -Editted 'GameController' Script to instantiate 'Hazard' GameObject, in this case 'Asteroid' Prefab with a random x position just above the visible area

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public Vector3 spawnValues;
+
+	// Use this for initialization
+	void Start () {
+        SpawnWaves();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    void SpawnWaves ()
+    {
+        // Spawns the Asteroid within a random range between x and -x, y, and z as specified by the inspector
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity; // Set Rotation to zero
+        Instantiate(hazard, spawnPosition, spawnRotation);
+    }
+}
