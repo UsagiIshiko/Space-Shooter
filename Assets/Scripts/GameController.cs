bfc3163244455805546648516b8a5c9612bbf47b
Uasgi Ishiko <usagisoftware@gmail.com>
2017-01-31 11:12:37 +0900

    2. Boundaries, Hazards and Enemies - 05. Spawning waves
    -Modified 'GameController' Script to allow the creation of waves of hazards, spawn wait times, game start wait times and wave wait times.
    -Created 'DestroyByTime' Script to add to all explosion Prefabs to allow for the clean up of 'explosion' GameObjects after 2 seconds of lifetime

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,26 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
     public GameObject hazard;
     public Vector3 spawnValues;
+    public int hazardCount = 1;
+    public float spawnWait = 1;
+    public float startWait = 1;
+    public float waveWait = 1;
 
-	// Use this for initialization
-	void Start () {
-        SpawnWaves();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    
-    void SpawnWaves ()
+    // Use this for initialization
+    void Start()
     {
-        // Spawns the Asteroid within a random range between x and -x, y, and z as specified by the inspector
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-        Quaternion spawnRotation = Quaternion.identity; // Set Rotation to zero
-        Instantiate(hazard, spawnPosition, spawnRotation);
+        StartCoroutine(SpawnWaves());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                // Spawns the Asteroid within a random range between x and -x, y, and z as specified by the inspector
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity; // Set Rotation to zero
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
     }
 }
